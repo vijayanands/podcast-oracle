78e32a484c5f4e2ad58dbe154d0f070dd19b01c5
Vijayanand Sankarasubramanian <email2vijay@gmail.com>
2024-05-31 14:16:28 -0700

    added aspect and sentiment extraction

---
+++ aspect_and_sentiment_extraction.py
@@ -0,0 +1,51 @@
+from import_envs import openai_api_key
+from import_envs import rtf_file
+from langchain.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_core.runnables.passthrough import RunnablePassthrough
+from langchain.schema import StrOutputParser
+import re
+
+# Define the function to clean and extract text from RTF content
+def extract_text_from_rtf(rtf_str):
+    # Remove RTF tags and control words
+    plain_text = re.sub(r'{\\[^{}]+}', '', rtf_str)
+    plain_text = re.sub(r'\\[a-z]+\s?', '', plain_text)
+    plain_text = plain_text.replace('\n', ' ').replace('\r', '')
+    return plain_text
+
+
+# Read the RTF file content
+with open(rtf_file, 'r') as file:
+    rtf_content = file.read()
+
+# Extract plain text from the RTF content
+document_text = extract_text_from_rtf(rtf_content)
+
+prompt = """
+
+Extract aspects from the given text {document}. Once the aspects are extracted print them as a bulleted list and then based on the nature of 
+aspects give a sentiment analysis on the aspects
+
+"""
+
+prompt_template = PromptTemplate(template=prompt, input_variables=["document"])
+
+# create a chat model / LLM
+chat_model = ChatOpenAI(
+    model="gpt-3.5-turbo", temperature=0, api_key=openai_api_key
+)
+
+# create a parser to parse the output of our LLM
+parser = StrOutputParser()
+
+# 💻 Create the sequence (recipe)
+runnable_chain = (
+    {"document": RunnablePassthrough()}
+    | prompt_template
+    | chat_model
+    | StrOutputParser()
+)
+
+answer = runnable_chain.invoke(document_text)
+print(answer)
--- model_utils.py
+++ model_utils.py
@@ -5,14 +5,18 @@ from langchain_community.llms import Ollama
 
 default_model = OpenAI(temperature=0, api_key=openai_api_key)
 
-def get_model():
-    if llm_model == "OPENAI":
+def get_model(model_override = None):
+    if model_override is not None:
+        model_str = model_override
+    else:
+        model_str = llm_model
+    if model_str == "OPENAI":
         llm = default_model
         print(f"Model Name: {llm.model_name}");
-    elif llm_model == "CLAUDE":
+    elif model_str == "CLAUDE":
         llm = ChatAnthropic(model_name="claude-2.1", anthropic_api_key=anthropic_api_key)
         print(f"Model Name: {llm.model}");
-    elif llm_model == "LLAMA3":
+    elif model_str == "LLAMA3":
         # Now you can use `llm` for generating responses, etc.
         llm = Ollama(model="llama3")
         print(f"Model Name: {llm.model}");
